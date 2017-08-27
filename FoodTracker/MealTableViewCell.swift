1a8240a6552cb25aaf377abd4e57d1845a7b6e01
nekomeo <linh.tu545@gmail.com>
2017-08-27 16:04:19 -0700

    Add UITableViewCell, photo, label, and stacked view to UITableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Elle Ti on 2017-08-27.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
