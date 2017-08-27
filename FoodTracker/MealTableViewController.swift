ffb289b74e5c00a0f6086b1077ea16687cfb0de9
nekomeo <linh.tu545@gmail.com>
2017-08-27 16:39:51 -0700

    Set cells in tableview

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -29,25 +29,40 @@ class MealTableViewController: UITableViewController
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return meals.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        // Table view cells are reused and should be dequeued using a cell identifier
+        let cellIdentifier = "MealTableViewCell"
+        
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else
+        {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell")
+        }
+        
+        // Fetches the appropriate meal for the data source layout
+        let meal = meals[indexPath.row]
 
         // Configure the cell...
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
