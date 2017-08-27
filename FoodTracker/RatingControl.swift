5fde5a86a7d6b0e5b668ee33ed448246817cb008
nekomeo <linh.tu545@gmail.com>
2017-08-27 00:38:57 -0700

    Add function for star rating

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -14,6 +14,12 @@ import UIKit
     private var ratingButtons = [UIButton]()
     
     var rating = 0
+    {
+        didSet
+        {
+            updateButtonSelectionStates()
+        }
+    }
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
     {
@@ -47,7 +53,25 @@ import UIKit
     //MARK: Button Action
     func ratingButtonTapped(button: UIButton)
     {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else
+        {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating
+        {
+            // If the selected star represents the current rating, reset the rating to 0
+            rating = 0
+        }
+        
+        else
+        {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
@@ -94,6 +118,15 @@ import UIKit
             
             // Add the new button to he rating button array
             ratingButtons.append(button)
+        }
+    }
+    
+    private func updateButtonSelectionStates()
+    {
+        for (index, buttton) in ratingButtons.enumerated()
+        {
+            // If the index of a button is less than the rating, that button should be selected
+            buttton.isSelected = index < rating
         }
     }
 
